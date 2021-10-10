de26b009064dcf77c2ac98d0cf17d4f8d29cab88
Phillip Wong <deepelegant99@gmail.com>
2021-10-10 13:55:46 -0700

    Using the data on the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,17 @@
+import projectData from "../data/data"
+
+
 const Projects = () => {
+    console.log(projectData)
+
     return (
-        <div>
-            Project Page
-        </div>
-    )
+      <>
+        <h1>Project Page</h1>
+        {projectData.map(project =>{
+            return <li>{project.Title}</li>
+        })}
+      </>
+    );
 }
 
 export default Projects
