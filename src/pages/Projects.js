ec9d07d833d1fe20f9942ba85fec16cd230874cf
Phillip Wong <deepelegant99@gmail.com>
2021-10-10 14:07:17 -0700

    Added the container in the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,17 +1,19 @@
-import projectData from "../data/data"
-
+import { Container, Grid } from "@material-ui/core";
+import projectData from "../data/data";
 
 const Projects = () => {
-    console.log(projectData)
+  console.log(projectData);
 
-    return (
-      <>
+  return (
+    <Container maxWidth="lg">
+      <Grid Container>
         <h1>Project Page</h1>
-        {projectData.map(project =>{
-            return <li>{project.Title}</li>
+        {projectData.map((project) => {
+          return <li>{project.Title}</li>;
         })}
-      </>
-    );
-}
+      </Grid>
+    </Container>
+  );
+};
 
-export default Projects
+export default Projects;
