4aadefe2a2c5971768e93ec3cbaf6a7ae5ffb8f1
Phillip Wong <deepelegant99@gmail.com>
2021-10-10 11:08:29 -0700

    added a data file to display projects

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+const projectData = [
+  {
+    Title: "Cat and Mouse",
+    URL: "www.google.com",
+    Image:
+      "https://www.victorpest.com/media/Articles/Victorpest/How-to-Train-Your-Cat-to-Hunt-Mice.jpg",
+    Description: "Something about cats and mouse with some react elements",
+  },
+  {
+    Title: "I did it again",
+    URL: "www.google.com",
+    Image:
+      "https://www.victorpest.com/media/Articles/Victorpest/How-to-Train-Your-Cat-to-Hunt-Mice.jpg",
+    Description: "Very popular song in the 90s",
+  },
+];
+
+export default projectData; 
